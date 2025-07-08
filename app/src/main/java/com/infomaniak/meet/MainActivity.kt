a982426fe08029f50a9aeec9507ccea3c6787ad8
Vincent TE <vincent.te@infomaniak.com>
2025-07-08 15:28:10 +0200

    chore: Remove unused import

--- app/src/main/java/com/infomaniak/meet/MainActivity.kt
+++ app/src/main/java/com/infomaniak/meet/MainActivity.kt
@@ -6,7 +6,6 @@ import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.View
 import android.view.View.GONE
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
