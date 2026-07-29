4c2d8948de94d4be0342c870f24c6a6692d638e1
Joris Bodin <joris.bodin@infomaniak.com>
2026-07-29 10:04:29 +0200

    chore(Version): 2.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.infomaniak.meet"
         minSdk = 27
         targetSdk = 37
-        versionCode = 28
-        versionName = "2.6.8"
+        versionCode = 29
+        versionName = "2.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
